b4f5ba9e673c5f5d3169724945bb998b1ab086d4
liwenxin <mxlwx@126.com>
2022-02-18 09:36:45 +0800

    update brandmanagement logic

--- src/components/BrandManagement.vue
+++ src/components/BrandManagement.vue
@@ -1,15 +1,68 @@
 <template>
-  <div>
+  <div class="brand">
+    <div class="panel panel-primary">
+      <div class="panel-heading">
+        <h3 class="panel-title">添加品牌</h3>
+      </div>
+    </div>
+    
+    <div class="panel-body">
+      <div class="form-group form-inline">
+        <div class="input-group pull-left">
+          <div class="input-group-addon">编号</div>
+          <input type="text" class="form-control" v-model="brand.id">
+        </div>
 
+        <div class="input-group">
+          <div class="input-group-addon">品牌名称</div>
+          <input type="text" class="form-control" v-model="brand.name">
+        </div>
+
+        <div class="input-group">
+          <button class="btn btn-primary" @click="addBrand">添加</button>
+        </div>
+
+        <div class="input-group pull-right">
+          <div class="input-group-addon">按名称搜索</div>
+          <input type="text" class="form-control">
+        </div>
+      </div>
+    </div>
+
+    <table class="table table-bordered table-hover table-">
+      <thead>
+        <tr>
+          <th>编号</th>
+          <th>品牌名称</th>
+          <th>添加时间</th>
+          <th>操作</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in brandList" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.ctime }}</td>
+          <td>
+            <a href="#">删除</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useBrandStore } from "../store/brandStore"
 import { storeToRefs } from "pinia"
-const { brandList } = storeToRefs(useBrandStore())
+import { useBrandStore } from "../store/brandStore"
+
+const { brandList, brand } = storeToRefs(useBrandStore())
+const { addBrand } = useBrandStore()
 </script>
 
 <style scoped>
-
+.brand {
+  width: 1000px;
+  margin: 0 auto;
+}
 </style>
--- src/store/brandStore.ts
+++ src/store/brandStore.ts
@@ -2,8 +2,32 @@ import { defineStore } from 'pinia'
 
 export const useBrandStore = defineStore('brand', {
   state: () => ({
-    brandList: [],
+    brandList: [
+      { id: '1', name: '奔驰', ctime: '2020-09-23' },
+      { id: '2', name: '宝马', ctime: '2020-09-21' },
+      { id: '3', name: '长安奔驰', ctime: '2014-06-23' },
+      { id: '4', name: '千里马', ctime: '2020-05-03' }
+    ], // 品牌列表字段
+    brand: {
+      id: '',
+      name: '',
+      ctime: new Date().toLocaleDateString()
+    } // 双向绑定字段
   }),
   // 声明getters
   // 声明actions
+  actions: {
+    addBrand() {
+      console.log(1111);
+      // 1. 把state定义的brand push brandList
+      this.brandList.push(this.brand)
+
+      // 2. 清空input中的内容
+      this.brand = {
+        id: '',
+        name: '',
+        ctime: new Date().toLocaleDateString()
+      }
+    }
+  }
 })
